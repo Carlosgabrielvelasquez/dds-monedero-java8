d15b8c01f5bb78f354aa5aa6552572f0283fc732
Carlos <velasquezcgm@gmail.com>
2021-05-11 21:36:57 -0300

    Agrego Test en los limites

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -1,5 +1,6 @@
 package dds.monedero.model;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,6 +8,8 @@ import dds.monedero.exceptions.MaximaCantidadDepositosException;
 import dds.monedero.exceptions.MaximoExtraccionDiarioException;
 import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
+
+import java.time.LocalDate;
 
 public class MonederoTest {
   private Cuenta cuenta;
@@ -31,6 +34,7 @@ public class MonederoTest {
     cuenta.poner(1500);
     cuenta.poner(456);
     cuenta.poner(1900);
+    Assert.assertEquals(3,cuenta.getMovimientos().stream().filter(movimiento -> movimiento.fueDepositado(LocalDate.now())).count());
   }
 
   @Test(expected = MaximaCantidadDepositosException.class)
@@ -47,10 +51,24 @@ public class MonederoTest {
     cuenta.sacar(1001);
   }
 
+  @Test
+  public void ExtraerTodoElSaldo() {
+    cuenta.setSaldo(90);
+    cuenta.sacar(90);
+    Assert.assertEquals(0,cuenta.getSaldo(),0.00001);
+  }
+
   @Test(expected = MaximoExtraccionDiarioException.class)
   public void ExtraerMasDe1000() {
     cuenta.setSaldo(5000);
     cuenta.sacar(1001);
+  }
+
+  @Test
+  public void ExtraeJusto1000() {
+    cuenta.setSaldo(5000);
+    cuenta.sacar(1000);
+    Assert.assertEquals(4000,cuenta.getSaldo(),0.00001);
   }
 
   @Test(expected = MontoNegativoException.class)
